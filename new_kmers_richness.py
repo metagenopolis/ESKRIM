2ae37d9a5bb2e40a8ecb0b288bfaebbb48269639
Florian Plaza Oñate <florian.plaza-onate@inra.fr>
2019-08-23 11:08:00 +0200

    New code

---
+++ new_kmers_richness.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+""""""
+
+from __future__ import print_function
+from __future__ import division
+import argparse
+import os
+import sys
+from collections import namedtuple
+import fileinput 
+import itertools
+import random
+import subprocess
+import multiprocessing
+import tempfile
+try:
+    import dna_jellyfish
+except ImportError as err:
+    raise RuntimeError('Python bindings of jellyfish are not installed')
+
+__author__ = "Florian Plaza Oñate"
+__copyright__ = "Copyright 2019, INRA"
+__maintainer__ = "Florian Plaza Oñate"
+__email__ = "florian.plaza-onate@inra.fr"
+__status__ = "Development"
+
+def check_program_available(program):
+    try:
+        with open(os.devnull, 'w') as devnull:
+            subprocess.call([program], stdout=devnull, stderr=devnull)
+    except OSError:
+        raise RuntimeError('{0} not found or not in system path'.format(program))
+
+def num_threads_type(value):
+    max_num_threads = multiprocessing.cpu_count()
+
+    try:
+        value = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError('NUM_THREADS is not an integer')
+
+    if value <= 0:
+        raise argparse.ArgumentTypeError('minimum NUM_THREADS is 1')
+    elif value > max_num_threads:
+        raise argparse.ArgumentTypeError('maximum NUM_THREADS is {}'.format(max_num_threads))
+
+    return value
+
+def get_parameters():
+    """Parse command line parameters.
+    """
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-i', dest='input_fastq_files', nargs='+', required=True,
+            help='INPUT_FASTQ_FILES with reads from a single metagenomic sample (gzip and bzip2 compression accepted)')
+
+    parser.add_argument('-l', dest='read_length', type=int, default=80,
+            help='discard reads shorter than READ_LENGTH bases and trim those exceeding this length')
+
+    parser.add_argument('-r', dest='num_reads', type=int, default=10000000,
+            help='NUM_READS to draw randomly from INPUT_FASTQ_FILES')
+
+    parser.add_argument('-k', dest='kmer_length', type=int, choices=range(17,32,2), default=21, 
+            help='length of kmers to count')
+
+    parser.add_argument('-t', dest='num_threads', type=num_threads_type, default=4, 
+            help='NUM_THREADS to launch for kmers counting')
+
+    parser.add_argument('-o', dest='output_stats_file', type=argparse.FileType('w'), required=True, 
+            help='OUTPUT_STATS_FILE with kmer richness estimates')
+
+    return parser.parse_args()
+
+FastqEntry=namedtuple('FastqEntry', ['name', 'seq', 'qual'])
+def fastq_reader(istream, target_read_length):
+    while istream:
+        name = istream.next().rstrip('\n')
+        seq = istream.next().rstrip('\n')
+        istream.next()
+        qual = istream.next().rstrip('\n')
+        if len(seq) >= target_read_length:
+            yield FastqEntry(name, seq[0:target_read_length], qual[0:target_read_length])
+
+def fastq_formatter(fastq_entry):
+    return '@{}\n{}\n+\n{}\n'.format(fastq_entry.name, fastq_entry.seq, fastq_entry.qual)
+
+def subsample_fastq_files(input_fastq_files, subsample_size, target_read_length):
+    fi = fileinput.FileInput(input_fastq_files, openhook=fileinput.hook_compressed)
+    # Fill reservoir
+    selected_reads = list(itertools.islice(fastq_reader(fi, target_read_length), subsample_size))
+
+    if len(selected_reads) < subsample_size:
+        raise RuntimeError('only {num_reads} reads of at least {read_length} bases are available in input FASTQ files.'.format(num_reads=len(selected_reads), read_length=target_read_length))
+
+    for total_num_reads, new_read in enumerate(fastq_reader(fi, target_read_length), start=subsample_size+1):
+        ind = random.randrange(0,total_num_reads)
+        if ind < subsample_size:
+            selected_reads[ind] = new_read
+
+    fi.close()
+
+    return total_num_reads, selected_reads
+
+
+def create_jf_db(reads, kmer_length, num_threads):
+    jellyfish_db_path = tempfile.mkstemp(dir = '/dev/shm', suffix = '.jf')[1]
+    jellyfish_count_cmd = ['jellyfish', 'count',
+                           '-C', '-m', str(kmer_length),
+                           '-s', '1G',
+                           '-t', str(num_threads),
+                           '-o', jellyfish_db_path,
+                           '/dev/stdin']
+
+    jellyfish_count_proc = subprocess.Popen(jellyfish_count_cmd, stdin=subprocess.PIPE)
+
+    for read in reads:
+        jellyfish_count_proc.stdin.write(fastq_formatter(read))
+
+    jellyfish_count_proc.stdin.close()
+    jellyfish_count_proc.wait()
+    
+    return jellyfish_db_path
+
+def count_distinct_kmers(jellyfish_db_path):
+    jellyfish_stats_cmd = ['jellyfish', 'stats',
+                           jellyfish_db_path]
+
+    jellyfish_stats_proc = subprocess.Popen(jellyfish_stats_cmd, stdout=subprocess.PIPE)
+
+    jellyfish_stats_proc.stdout.readline()
+    num_distinct_kmers = jellyfish_stats_proc.stdout.readline()
+    num_distinct_kmers = int(num_distinct_kmers.split()[-1])
+
+    jellyfish_stats_proc.wait()
+
+    return num_distinct_kmers
+
+def count_solid_kmers(jellyfish_db_path):
+    jellyfish_stats_cmd = ['jellyfish', 'stats',
+                           '-L', '2',
+                           jellyfish_db_path]
+
+    jellyfish_stats_proc = subprocess.Popen(jellyfish_stats_cmd, stdout=subprocess.PIPE)
+
+    jellyfish_stats_proc.stdout.readline()
+    num_solid_kmers = jellyfish_stats_proc.stdout.readline()
+    num_solid_kmers = int(num_solid_kmers.split()[-1])
+
+    jellyfish_stats_proc.wait()
+
+    return num_solid_kmers
+
+def count_mercy_kmers(reads, jellyfish_db_path, read_length, kmer_length):
+    num_mercy_kmers = 0
+    jellyfish_db = dna_jellyfish.QueryMerFile(jellyfish_db_path)
+    read_num_kmers = read_length - kmer_length + 1
+
+    for read in reads:
+        cur_read_kmers_all_unique = True
+        for i in range(0, read_num_kmers):
+            mer = dna_jellyfish.MerDNA(read.seq[i:(i+kmer_length)])
+            mer.canonicalize()
+            cur_read_kmers_all_unique = bool(jellyfish_db[mer] == 1)
+            
+            if not cur_read_kmers_all_unique:
+                break
+
+        if cur_read_kmers_all_unique:
+            num_mercy_kmers += read_num_kmers
+
+    return num_mercy_kmers
+
+def print_output_stats(parameters, num_distinct_kmers, num_solid_kmers, num_mercy_kmers):
+    print('num_reads\tread_length\tk\tnum_distinct_kmers\tnum_solid_kmers\tnum_mercy_kmers', file=parameters.output_stats_file)
+    print('\t'.join([
+        str(parameters.num_reads),
+        str(parameters.read_length),
+        str(parameters.kmer_length),
+        str(num_distinct_kmers),
+        str(num_solid_kmers),
+        str(num_mercy_kmers)]),
+        file=parameters.output_stats_file)
+
+def main():
+    parameters = get_parameters()
+
+    check_program_available('jellyfish')
+
+    print('Subsampling reads from FASTQ files...')
+    total_num_reads, selected_reads = subsample_fastq_files(parameters.input_fastq_files, parameters.num_reads, parameters.read_length)
+    print('Done. {num_selected_reads} reads out of {total_num_reads} selected ({proportion}%).\n'.format(
+        num_selected_reads=len(selected_reads),
+        total_num_reads=total_num_reads,
+        proportion=round(100.0*len(selected_reads)/total_num_reads, 2)))
+
+    print('Creating jellyfish database...')
+    jellyfish_db_path = create_jf_db(selected_reads, parameters.kmer_length, parameters.num_threads)
+    print('Done.\n')
+
+    print('Counting distinct kmers...')
+    num_distinct_kmers = count_distinct_kmers(jellyfish_db_path)
+    m_distinct_kmers=0
+    print('Done.\n')
+
+    print('Counting solid kmers...')
+    num_solid_kmers = count_solid_kmers(jellyfish_db_path)
+    print('Done.\n')
+
+    print('Counting mercy kmers...')
+    num_mercy_kmers = count_mercy_kmers(selected_reads, jellyfish_db_path, parameters.read_length, parameters.kmer_length)
+    print('Done.\n')
+
+    print('Printing output stats...')
+    print_output_stats(parameters, num_distinct_kmers, num_solid_kmers, num_mercy_kmers)
+    print('Done.\n')
+
+    print('Cleanup...')
+    os.remove(jellyfish_db_path)
+    print('Done.')
+
+if __name__ == '__main__':
+    main()
+
