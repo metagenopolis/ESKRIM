fab4c36fb755257fba92d1a8be275073a51d6db9
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2021-09-06 15:38:13 +0200

    Update version and happy new year

--- eskrim.py
+++ eskrim.py
@@ -21,12 +21,12 @@ except ImportError as err:
     raise RuntimeError('Python bindings of jellyfish are not installed')
 
 __author__ = "Florian Plaza Oñate"
-__copyright__ = "Copyright 2019-2020, INRAE"
+__copyright__ = "Copyright 2019-2021, INRAE"
 __maintainer__ = "Florian Plaza Oñate"
 __email__ = "florian.plaza-onate@inrae.fr"
 __status__ = "Production"
 __licence__= "GNU GPLv3"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
