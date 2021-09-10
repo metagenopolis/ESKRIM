61ae1141db54914c055902cd406464c243735ff6
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2021-09-10 14:53:13 +0200

    Standardize version message.\nReformat code to follow PEP8 recommendations

--- eskrim.py
+++ eskrim.py
@@ -8,7 +8,7 @@ import os
 import sys
 import re
 from collections import namedtuple
-import fileinput 
+import fileinput
 import itertools
 import random
 import subprocess
@@ -25,11 +25,15 @@ __copyright__ = "Copyright 2019-2021, INRAE"
 __maintainer__ = "Florian Plaza Oñate"
 __email__ = "florian.plaza-onate@inrae.fr"
 __status__ = "Production"
-__licence__= "GNU GPLv3"
-__version__ = "1.0.2"
+__licence__ = "GNU GPLv3"
+__version__ = "1.0.3"
+
+eskrim_version = f'eskrim {__version__}'
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
 
 def check_program_available(program):
     try:
@@ -37,6 +41,7 @@ def check_program_available(program):
             subprocess.call([program], stdout=devnull, stderr=devnull)
     except OSError:
         raise RuntimeError('{0} not found or not in system path'.format(program))
+
 
 def num_threads_type(value):
     max_num_threads = multiprocessing.cpu_count()
@@ -53,41 +58,43 @@ def num_threads_type(value):
 
     return value
 
+
 def get_parameters():
     """Parse command line parameters.
     """
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('-i', dest='input_fastq_files', nargs='+', required=True,
-            help='INPUT_FASTQ_FILES with reads from a single metagenomic sample (gzip and bzip2 compression accepted)')
+                        help='INPUT_FASTQ_FILES with reads from a single metagenomic sample (gzip and bzip2 compression accepted)')
 
     parser.add_argument('-n', dest='sample_name', default='NA',
-            help='name of the metagenomic sample')
+                        help='name of the metagenomic sample')
 
     parser.add_argument('-l', dest='read_length', type=int, default=80,
-            help='discard reads shorter than READ_LENGTH bases and trim those exceeding this length')
+                        help='discard reads shorter than READ_LENGTH bases and trim those exceeding this length')
 
     parser.add_argument('-r', dest='num_reads', type=int, default=10000000,
-            help='NUM_READS to draw randomly from INPUT_FASTQ_FILES')
+                        help='NUM_READS to draw randomly from INPUT_FASTQ_FILES')
 
-    parser.add_argument('-k', dest='kmer_length', type=int, choices=range(17,32,2), default=21, 
-            help='length of kmers to count')
+    parser.add_argument('-k', dest='kmer_length', type=int, choices=range(17, 32, 2), default=21,
+                        help='length of kmers to count')
 
-    parser.add_argument('-t', dest='num_threads', type=num_threads_type, default=4, 
-            help='NUM_THREADS to launch for kmers counting')
+    parser.add_argument('-t', dest='num_threads', type=num_threads_type, default=4,
+                        help='NUM_THREADS to launch for kmers counting')
 
     parser.add_argument('-o', dest='output_fastq_file', type=argparse.FileType('w'),
-            help='OUTPUT_FASTQ_FILE with the randomly selected reads')
+                        help='OUTPUT_FASTQ_FILE with the randomly selected reads')
 
-    parser.add_argument('-s', dest='output_stats_file', type=argparse.FileType('w'), required=True, 
-            help='OUTPUT_STATS_FILE with kmer richness estimates')
+    parser.add_argument('-s', dest='output_stats_file', type=argparse.FileType('w'), required=True,
+                        help='OUTPUT_STATS_FILE with kmer richness estimates')
 
     parser.add_argument('--seed', dest='rng_seed', type=int, default=0,
-            help='Seed for random number generator ')
+                        help='Seed for random number generator ')
 
-    parser.add_argument('--version', action='version', version=f'eskrim {__version__}')
+    parser.add_argument('--version', action='version', version=eskrim_version)
 
     return parser.parse_args()
+
 
 def hook_compressed_text(filename, mode, encoding='utf8'):
     ext = os.path.splitext(filename)[1]
@@ -100,19 +107,22 @@ def hook_compressed_text(filename, mode, encoding='utf8'):
     else:
         return open(filename, mode, encoding=encoding)
 
+
 def check_fastq_files(fastq_files):
     fastq_extensions = {'.fastq', '.fq', '.fastq.gz', '.fq.gz', '.fastq.bz2', '.fq.bz2'}
-    regexp_match_fastq_extensions = '({all_extensions})$'.format(all_extensions = '|'.join(str.replace(fastq_extension, '.', '\.') for fastq_extension in fastq_extensions))
-    problematic_fastq_files = [os.path.basename(fastq_file) for fastq_file in fastq_files if re.sub(regexp_match_fastq_extensions, '', fastq_file).endswith(('.2','_R2'))]
+    regexp_match_fastq_extensions = '({all_extensions})$'.format(all_extensions='|'.join(str.replace(fastq_extension, '.', '\.') for fastq_extension in fastq_extensions))
+    problematic_fastq_files = [os.path.basename(fastq_file) for fastq_file in fastq_files if re.sub(regexp_match_fastq_extensions, '', fastq_file).endswith(('.2', '_R2'))]
 
     if problematic_fastq_files:
         eprint('warning: some files probably contain reverse reads ({})'.format(','.join(problematic_fastq_files)))
         eprint('warning: use only forward reads for accurate results\n')
 
-FastqEntry=namedtuple('FastqEntry', ['name', 'seq', 'qual'])
+FastqEntry = namedtuple('FastqEntry', ['name', 'seq', 'qual'])
+
+
 def fastq_reader(istream, target_read_length):
     while istream:
-        try :
+        try:
             name = next(istream).rstrip('\n')
         except StopIteration:
             return
@@ -121,7 +131,7 @@ def fastq_reader(istream, target_read_length):
         qual = next(istream).rstrip('\n')
         fastq_reader.total_num_reads += 1
 
-        if 'N' in seq :
+        if 'N' in seq:
             fastq_reader.num_Ns_reads_ignored += 1
         elif len(seq) < target_read_length:
             fastq_reader.num_too_short_reads_ignored += 1
@@ -132,8 +142,10 @@ fastq_reader.total_num_reads = 0
 fastq_reader.num_Ns_reads_ignored = 0
 fastq_reader.num_too_short_reads_ignored = 0
 
+
 def fastq_formatter(fastq_entry):
     return f'@{fastq_entry.name}\n{fastq_entry.seq}\n+\n{fastq_entry.qual}\n'
+
 
 def subsample_fastq_files(input_fastq_files, subsample_size, target_read_length):
     with fileinput.FileInput(input_fastq_files, openhook=hook_compressed_text) as fi:
@@ -144,14 +156,15 @@ def subsample_fastq_files(input_fastq_files, subsample_size, target_read_length)
             eprint('warning: only {num_reads} reads with no Ns of at least {read_length} bases are available in input FASTQ files.'.format(num_reads=len(selected_reads), read_length=target_read_length))
 
         for new_read in fastq_reader(fi, target_read_length):
-            ind = random.randrange(0,fastq_reader.total_num_reads)
+            ind = random.randrange(0, fastq_reader.total_num_reads)
             if ind < subsample_size:
                 selected_reads[ind] = new_read
 
     return selected_reads
 
+
 def create_jf_db(reads, kmer_length, num_threads):
-    jellyfish_db_path = tempfile.mkstemp(dir = '/dev/shm', suffix = '.jf')[1]
+    jellyfish_db_path = tempfile.mkstemp(dir='/dev/shm', suffix='.jf')[1]
     jellyfish_count_cmd = ['jellyfish', 'count',
                            '-C', '-m', str(kmer_length),
                            '-s', '1G',
@@ -166,8 +179,9 @@ def create_jf_db(reads, kmer_length, num_threads):
 
     jellyfish_count_proc.stdin.close()
     jellyfish_count_proc.wait()
-    
+
     return jellyfish_db_path
+
 
 def count_distinct_kmers(jellyfish_db_path):
     jellyfish_stats_cmd = ['jellyfish', 'stats',
@@ -182,6 +196,7 @@ def count_distinct_kmers(jellyfish_db_path):
     jellyfish_stats_proc.wait()
 
     return num_distinct_kmers
+
 
 def count_solid_kmers(jellyfish_db_path):
     jellyfish_stats_cmd = ['jellyfish', 'stats',
@@ -198,6 +213,7 @@ def count_solid_kmers(jellyfish_db_path):
 
     return num_solid_kmers
 
+
 def count_mercy_kmers_aux(params):
         reads, jellyfish_db_path, read_length, kmer_length = params
         num_mercy_kmers = 0
@@ -210,7 +226,7 @@ def count_mercy_kmers_aux(params):
                 mer = dna_jellyfish.MerDNA(read.seq[i:(i+kmer_length)])
                 mer.canonicalize()
                 cur_read_kmers_all_unique = bool(jellyfish_db[mer] == 1)
-                
+
                 if not cur_read_kmers_all_unique:
                     break
 
@@ -219,14 +235,16 @@ def count_mercy_kmers_aux(params):
 
         return num_mercy_kmers
 
-def count_mercy_kmers(reads, jellyfish_db_path, read_length, kmer_length, num_threads, chunksize = 200000):
-    chunks_parameters = ((reads[x:x+chunksize],jellyfish_db_path, read_length, kmer_length)
-            for x in range(0, len(reads), chunksize))
+
+def count_mercy_kmers(reads, jellyfish_db_path, read_length, kmer_length, num_threads, chunksize=200000):
+    chunks_parameters = ((reads[x:x+chunksize], jellyfish_db_path, read_length, kmer_length)
+                         for x in range(0, len(reads), chunksize))
     with multiprocessing.Pool(num_threads) as pool:
         chunks_num_mercy_kmers = pool.imap_unordered(count_mercy_kmers_aux, chunks_parameters)
         final_num_mercy_kmers = sum(chunks_num_mercy_kmers)
-    
+
     return final_num_mercy_kmers
+
 
 def main():
     parameters = get_parameters()
@@ -235,7 +253,7 @@ def main():
     check_fastq_files(parameters.input_fastq_files)
     random.seed(parameters.rng_seed)
 
-    print('ESKRIM v{version}\n'.format(version = __version__))
+    print(f'{eskrim_version}\n')
 
     print('Subsampling reads from FASTQ files...')
     selected_reads = subsample_fastq_files(parameters.input_fastq_files, parameters.num_reads, parameters.read_length)
@@ -260,7 +278,7 @@ def main():
 
     print('Counting distinct kmers...')
     num_distinct_kmers = count_distinct_kmers(jellyfish_db_path)
-    m_distinct_kmers=0
+    m_distinct_kmers = 0
     print('Done.\n')
 
     print('Counting solid kmers...')
@@ -272,19 +290,30 @@ def main():
     print('Done.\n')
 
     print('Printing output stats...')
-    print('sample_name\ttotal_num_reads\tnum_Ns_reads_ignored\tnum_too_short_reads_ignored\tnum_selected_reads\tread_length\tkmer_length\tnum_distinct_kmers\tnum_solid_kmers\tnum_mercy_kmers', file=parameters.output_stats_file)
     print('\t'.join([
-        parameters.sample_name,
-        str(fastq_reader.total_num_reads),
-        str(fastq_reader.num_Ns_reads_ignored),
-        str(fastq_reader.num_too_short_reads_ignored),
-        str(len(selected_reads)),
-        str(parameters.read_length),
-        str(parameters.kmer_length),
-        str(num_distinct_kmers),
-        str(num_solid_kmers),
-        str(num_mercy_kmers)]),
-        file=parameters.output_stats_file)
+          'sample_name',
+          'total_num_reads',
+          'num_Ns_reads_ignored',
+          'num_too_short_reads_ignored',
+          'num_selected_reads',
+          'read_length',
+          'kmer_length',
+          'num_distinct_kmers',
+          'num_solid_kmers',
+          'num_mercy_kmers']),
+          file=parameters.output_stats_file)
+    print('\t'.join([
+          parameters.sample_name,
+          str(fastq_reader.total_num_reads),
+          str(fastq_reader.num_Ns_reads_ignored),
+          str(fastq_reader.num_too_short_reads_ignored),
+          str(len(selected_reads)),
+          str(parameters.read_length),
+          str(parameters.kmer_length),
+          str(num_distinct_kmers),
+          str(num_solid_kmers),
+          str(num_mercy_kmers)]),
+          file=parameters.output_stats_file)
     parameters.output_stats_file.close()
     print('Done.\n')
 
